6bdf7365f42cd57f25d21e13254b72710d7c2dcc
HugoSart <hugo_sart@hotmail.com>
2018-09-26 02:43:33 -0300

    F I N A L I Z A D O .

--- carv/__main__.py
+++ carv/__main__.py
@@ -1,11 +1,43 @@
-from carv.car import Evaluator
+from carv.bayesian_network import BayesianNetwork
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--asia",   type=int)
+    parser.add_argument("--smoke",  type=int)
+    parser.add_argument("--tub",    type=int)
+    parser.add_argument("--lung",   type=int)
+    parser.add_argument("--bronc",  type=int)
+    parser.add_argument("--either", type=int)
+    parser.add_argument("--xray",   type=int)
+    parser.add_argument("--dysp",   type=int)
+    return parser.parse_args()
+
+
+def make_observations(args, evaluator):
+    if args.asia is not None:   evaluator.Nodes.asia.observe(args.asia)
+    if args.smoke is not None:  evaluator.Nodes.smoke.observe(args.smoke)
+    if args.tub is not None:    evaluator.Nodes.tub.observe(args.tub)
+    if args.lung is not None:   evaluator.Nodes.lung.observe(args.lung)
+    if args.bronc is not None:  evaluator.Nodes.bronc.observe(args.bronc)
+    if args.either is not None: evaluator.Nodes.either.observe(args.either)
+    if args.xray is not None:   evaluator.Nodes.xray.observe(args.xray)
+
 
 def __main__():
+    # Interpreta argumentos
+    args = parse_arguments()
 
-    evaluator = Evaluator()
-    evaluator.parse_data_csv('data/car.csv')
+    # Cria rede bayesiana referente ao problema
+    evaluator = BayesianNetwork()
     evaluator.create_network()
-    evaluator.print()
+
+    # Faz as observações na rede baseadas nos argumentos do programa
+    make_observations(args, evaluator)
+
+    # Roda as inferencias e printa os resultados
+    evaluator.run_inference()
+    evaluator.show_result_as_table()
 
 
 __main__()
---
+++ carv/bayesian_network.py
@@ -0,0 +1,82 @@
+from bayespy.nodes import Categorical, Mixture, MultiMixture
+from bayespy.inference import VB
+
+FALSE = 0
+TRUE = 1
+
+
+class BayesianNetwork:
+    class Nodes(enumerate):
+        asia = Categorical
+        smoke = Categorical
+        tub = Mixture
+        lung = Mixture
+        bronc = Mixture
+        either = MultiMixture
+        xray = Mixture
+        dysp = MultiMixture
+
+    def create_network(self):
+        print("LOG -> Creating bayesian network ...")
+        n = self.Nodes
+
+        ap1 = 0.9999999999999999
+        ap0 = 0.0000000000000001
+
+        n.asia = Categorical([0.01, 0.99])
+        n.smoke = Categorical([0.5, 0.5])
+        n.tub = Mixture(n.asia, Categorical, [[0.99, 0.01], [0.95, 0.05]])
+        n.lung = Mixture(n.smoke, Categorical, [[0.99, 0.01], [0.9, 0.1]])
+        n.bronc = Mixture(n.smoke, Categorical, [[0.7, 0.3], [0.4, 0.6]])
+        n.either = MultiMixture((n.lung, n.tub), Categorical, [[[ap1, ap0], [ap0, ap1]], [[ap0, ap1], [ap0, ap1]]])
+        n.xray = Mixture(n.either, Categorical, [[0.95, 0.05], [0.02, 0.98]])
+        n.dysp = MultiMixture((n.bronc, n.either), Categorical, [[[0.1, 0.9], [0.3, 0.7]], [[0.2, 0.8], [0.1, 0.9]]])
+
+        print("LOG -> Bayesian network successfully created!")
+
+    def show_result_as_table(self):
+        n = self.Nodes
+
+        precision = "%.2f"
+        space = "           | "
+        space_after = "          |"
+
+        print("\n================ BAYESIAN NETWORK ================\n")
+        print("    + -------------------------------------- +")
+        print("    |          TABELA DE INFERÊNCIAS         |")
+        print("    + -------- + -------------- + ---------- +")
+        print("    | NÓ       | PROBABILIDADE  | OBSERVADO  |")
+        print("    + -------- + -------------- + ---------- +")
+        print("    | asia     | " + precision % n.asia.get_moments()[0][TRUE] + space + [" ", "x"][
+            n.asia.observed] + space_after)
+        print("    | smoke    | " + precision % n.smoke.get_moments()[0][TRUE] + space + [" ", "x"][
+            n.smoke.observed] + space_after)
+        print("    | tub      | " + precision % n.tub.get_moments()[0][TRUE] + space + [" ", "x"][
+            n.tub.observed] + space_after)
+        print("    | lung     | " + precision % n.lung.get_moments()[0][TRUE] + space + [" ", "x"][
+            n.lung.observed] + space_after)
+        print("    | bronc    | " + precision % n.bronc.get_moments()[0][TRUE] + space + [" ", "x"][
+            n.bronc.observed] + space_after)
+        print("    | either   | " + precision % n.either.get_moments()[0][TRUE] + space + [" ", "x"][
+            n.either.observed] + space_after)
+        print("    | xray     | " + precision % n.xray.get_moments()[0][TRUE] + space + [" ", "x"][
+            n.xray.observed] + space_after)
+        print("    | dysp     | " + precision % n.dysp.get_moments()[0][TRUE] + space + [" ", "x"][
+            n.dysp.observed] + space_after)
+        print("    + -------- + -------------- + ---------- +")
+        print("\n==================================================\n")
+
+    def run_inference(self):
+        print("LOG -> Running inference ...")
+        n = self.Nodes
+
+        Q = VB(
+            n.dysp,
+            n.xray,
+            n.either,
+            n.bronc,
+            n.lung,
+            n.tub,
+            n.smoke,
+            n.asia)
+        Q.update(repeat=100)
